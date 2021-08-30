788e046242f13f1f3d1067d71cf2819ce5f541df
Mizan Al-sabbih <74708492+Mizan-Al-Sabbih@users.noreply.github.com>
2021-08-30 12:23:32 +0000

    Changed some files.

---
+++ src/install/install.js
@@ -0,0 +1,118 @@
+const { spawn } = require('child_process');
+const inquirer = require('inquirer');
+const arg = require('arg');
+
+function argument() {
+  const args = arg(
+    {
+      "--save-dev": Boolean,
+      "--save-prod": Boolean,
+      "--global": Boolean,
+      "--no-bin-links": Boolean,
+      "-D": "--save-dev",
+      "-P": "--save-prod",
+      "-g": "--global",
+    },
+    {
+       argv: process.argv.slice(2),
+    }
+  );
+  return {
+    saveInDevdependencies: args['--save-dev', '--save-prod'] || false,
+    installGlobally: args['--global'] || false,
+    installAnyPackage: args._[0],
+    notCreateAnyBinDirectory: args['--no-bin-links'] || false,
+  }
+}
+
+async function workingFunction(arguments) {
+  if (arguments.installAnyPackage ) {
+    return {
+      ... arguments,
+      installPackage: arguments.installAnyPackage,
+    }
+  }
+
+  const questions = [];
+  
+  if (!arguments.installAnyPackage) {
+   questions.push({
+     type: 'text',
+     name: 'package',
+     message: 'What package do you want to install?',
+   });
+}
+
+  questions.push({
+    type: 'list',
+    name: 'packageManager',
+    message: 'Which package manager do you want to use?',
+    choices: [
+      'NPM',
+      'Yarn'
+    ]
+  });
+  
+  const answers = await inquirer.prompt(questions);
+  
+  switch (answers.packageManager) {
+    case'NPM':
+       npmInstall();
+      break;
+    case'Yarn':
+       yarnInstall();
+      break;
+    default:
+      break;
+  }
+}
+
+function cli(args) {
+  let arguments = argument(args);
+  arguments = workingFunction(args);
+  console.log(arguments);
+}
+
+cli();
+
+function npmInstall() {
+  const npm = spawn('npm', ['install', `${arguments.installAnyPackage || answers.package} ${arguments}`]);
+  npm.stderr.on('data', (data) => {
+   console.log(data);
+ });
+  npm.stdout.on('data', (data) => {
+   if (fs.existsSync(path.join('/node_modules', answers.package))) {
+    console.log("Updating package configuration \n \n" + data);
+   }
+    console.log("Installing package \n \n" + data);
+ });
+   npm.on('error', (error) => {
+    console.log(error);
+ });
+  npm.on('close', (code) => {
+    if (code){
+     console.log(`Process exited with code: ${code}`);
+   };
+ });
+}
+
+function npmInstall(answers) {
+  const yarn = spawn('yarn', ['add', `${arguments.installAnyPackage || answers.package} ${arguments}`]);
+  yarn.stderr.on('data', (data) => {
+   console.log(data);
+ });
+  yarn.stdout.on('data', (data) => {
+   if (fs.existsSync(path.join('/node_modules', answers.package))) {
+    console.log("Updating package configuration \n \n" + data);
+   }
+    console.log("Installing package \n \n" + data);
+ });
+   yarn.on('error', (error) => {
+    console.log(error);
+ });
+  yarn.on('close', (code) => {
+    if (code){
+     console.log(`Process exited with code: ${code}`);
+   };
+ });
+}
